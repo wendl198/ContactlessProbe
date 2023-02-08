aa3230dfe7e75e24080d5612e248c8414f82fb45
wendl198 <124744550+wendl198@users.noreply.github.com>
2023-02-08 16:12:39 -0600

    Add files via upload

--- ContactlessScript.py
+++ ContactlessScript.py
@@ -9,13 +9,15 @@ save_path = ('C:\\Users\\mpms\\Desktop\\Contactless Probe\\RawData')
 parameter_path = 'C:\\Users\\mpms\\Desktop\\Contactless Probe\\parameters.dat'
 
 
+avg_num = 4 #this is the number times the lock in will get each voltage before the average is reported
+
 #Save data file
 file_input = input("Please type the file name here: ")
 filename = file_input + ".dat"
 completeName = os.path.join(save_path, filename)
 
 file = open(completeName, "a")
-file.write("Time (sec)" + "\t" + "T (K)" + "\t" + "T_sample(K)" + "\t" + "R" + "\t" + "Th" + "\t" + "V")
+file.write("Time (sec)" + "\t" + "T (K)" + "\t" + "Vx (V)" + "\t" + "Vy (V)" + "\t" + "Th" + "\t" + "V")
 file.write("\n")
 file = completeName
 
@@ -47,111 +49,91 @@ class Parameter_Reader:
         del RT1
         return Stop_flag
 
-
 #get intitial parameters
 rrate = Parameter_Reader.rrate()
 finalT = Parameter_Reader.finalT()
 Stop_flag = Parameter_Reader.Stop_flag()
 
+#initialize plots
 fig = plt.figure(constrained_layout = True)
-ax = fig.add_subplot(1, 3, 1)
-bx = fig.add_subplot(1, 3, 2)
-cx = fig.add_subplot(1, 3, 3)
+ax = fig.add_subplot(2, 2, 1)
+bx = fig.add_subplot(2, 2, 2)
+cx = fig.add_subplot(2, 2, 3)
+dx = fig.add_subplot(2, 2, 3)
+fig.suptitle('Resistance measurement')
+#fig.suptitle('Resistivity measurement', fontname = "Times New Roman", fontweight = 'bold', fontsize = 20)
+ax.set_xlabel('Time (s)')
+ax.set_ylabel('Temperature (K)')
+bx.set_xlabel('Time (s)')
+bx.set_ylabel('Heater Percent (%)')
+cx.set_xlabel('Time (s)')
+cx.set_ylabel('Vx (?V)')
+dx.set_xlabel('Time (s)')
+dx.set_ylabel('Vx (?V)')
 
-
-
-TA = float(ls.query('KRDG? a'))
-time.sleep(0.05)
-TB = float(ls.query('KRDG? b'))
-time.sleep(0.05)
-
+#set intial lakeshore parameters
 ls.write('RAMP 1,1,%.2f' % rrate)
 time.sleep(0.05)
 ls.write('SETP 1,%.2f' % finalT)
 time.sleep(0.05)
 
+
+values = {}
+
+#main loop
 while Stop_flag == 0:
     time.sleep(0.05)
-    values = {}
-    xa,ya,xb,yb,vx, vy, vr, vt = [[],[],[],[],[],[],[], []]
-    del Stop_flag
     
-    time1 = time.perf_counter()
+    del Stop_flag #all local variables are deleted to prevent data pileup in secondary memory that slows processes down
+    
+    values['time'] = time.perf_counter()
 
     rrate = Parameter_Reader.rrate()
     finalT = Parameter_Reader.finalT()
     Stop_flag = Parameter_Reader.Stop_flag()
 
-    TA_temp = ls.query('KRDG? a')
-    TA = float(TA_temp)
-    time.sleep(0.1)
-    TB_temp = ls.query('KRDG? b')
-    TB = float(TB_temp)
+    values['Temp'] = float(ls.query('KRDG? a'))
+    if finalT <= values['Temp']:
+        Stop_flag = 1
+
+    time.sleep(0.05)
+    # values['T_sample'] = float(ls.query('KRDG? b'))
+    # time.sleep(0.05)
+    values['heater'] = float(ls.query('HTR? 1'))
     time.sleep(0.05)
     ls.write('RAMP 1,1,%.2f' % rrate)
     time.sleep(0.05)
     ls.write('SETP 1,%.2f' % finalT)
     time.sleep(0.05)
 
-   
-    for i in range(4):
+    vx, vy=0
+    for i in range(avg_num):
         p = srs.query('SNAPD?')
-        p1 = p.split(',')
-        vx_temp = float(p1[0])
-        vy_temp = float(p1[1])
-        vr_temp = float(p1[2])
-        vt_temp = float(p1[3])
-        vx.append(vx_temp)
-        vy.append(vy_temp)
-        vr.append(vr_temp)
-        vt.append(vt_temp)
-        del vx_temp, vy_temp, vr_temp, vt_temp
+        p1 = p.split(',') #this is [Vx, Vy, Resistance, Theta]
+        vx += float(p1[0])/avg_num
+        vy += float(p1[1])/avg_num
+        del p, p1
         time.sleep(0.1)
-
-    vx_final = sum(vx)/4
-    vy_final = sum(vy)/4
-    vr_final = sum(vr)/4
-    vt_final = sum(vt)/4
-    v_final = math.sqrt((vx_final)**2+(vy_final)**2)
-
-    values['time'] = time1
-    values['T'] = TA
-    values['T_sample'] = TB
-    values['R'] = vr_final
-    values['Th'] = vt_final
-    values['V_total'] = v_final 
-
-    #fig.suptitle('Resistivity measurement', fontname = "Times New Roman", fontweight = 'bold', fontsize = 20)
-    fig.suptitle('Resistance measurement')
-    xa.append(values['T'])
-    ya.append(values['R'])
-
-    xb.append(values['time'])
-    yb.append(values['R'])
-
-    ax.plot(xa,ya,'bo--')
-    ax.set_xlabel('Temperature (K)')
-    ax.set_ylabel('R')
+    values['Vx'] = vx
+    values['Vy'] = vy
+    ax.plot(values['time'],values['Temp'],'bo--')
+    bx.plot(values['time'],values['heater'],'ro--')
+    cx.plot(values['time'],values['Vx'],'bo--')
+    dx.plot(values['time'],values['Vy'],'bo--')
    
-    bx.plot(xb,yb,'bo--')
-    bx.set_xlabel('Time')
-    bx.set_ylabel('R')
-   
-    plt.pause(0.1)
-
+    plt.pause(0.1) #this displays the graph
     time.sleep(0.1)
 
+    #save data
     file = open(completeName, "a")
-    file.write(str(values['time']) + "\t" +  str(values['T']) + "\t" + str(values['T_sample']) + "\t" + str(values['R'])   + "\t" + str(values['Th']) + "\t" + str(values['V_total']))
-    file.write("\n")
+    file.write(str(values['time']) + "\t" +  str(values['Temp']) + "\t" + str(values['Vx']) + "\t" + str(values['Vy'])+"\n")
     file.flush()
     time.sleep(0.1)
 
-    del time1,TA, TB, vx_final, vr_final, vy_final, vt_final, values['time'],values['T'],values['T_sample'],values['R'],values['Th'], values['V_total']
-    del xa,ya,xb,yb,vx, vy, vr, vt
+    del values['time'],values['Temp'],values['Vx'],values['Vy'],values['heater']#,values['T_sample'],values['R'],values['Th'], values['V_total']
+    del vx, vy#, vr, vt
     file.close()
-   
-print("measurement_done")
+# print("measurement_done")
 ls.close()
 srs.close()
 #sr.close()
