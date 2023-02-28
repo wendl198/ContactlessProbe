0df10e62d7992497ec56dfa3d46b6a053d1d8360
wendl198 <124744550+wendl198@users.noreply.github.com>
2023-02-28 17:49:03 -0600

    Add files via upload

--- ContactlessScript.py
+++ ContactlessScript.py
@@ -4,55 +4,78 @@ import pyvisa
 import time
 import os
 import warnings
+from scipy.optimize import curve_fit
 
-def get_parameters():
-    with open(parameter_path) as f:
+def get_parameters(f):
+    try:
         lines = f.readlines()
-    del f
-    lines[6] = lines[6].split()
-    lines[6].pop(0) #this removes the word, but still returns a list
-    return (lines[0].split()[1],#RampRate
-            lines[1].split()[1],#StartTemp
-            lines[2].split()[1],#FinalTemp
-            int(lines[3].split()[1]),#RampingStatus
-            [lines[4].split()[1],lines[4].split()[2],lines[4].split()[3]],#PID paratmeters
-            int(lines[5].split()[1]),#Autoscale boolean
-            lines[6]) #time scale
+        lines[6] = lines[6].split()
+        lines[6].pop(0) #this removes the word, but still returns a list
+        return (lines[0].split()[1],#RampRate
+                lines[1].split()[1],#StartTemp
+                lines[2].split()[1],#FinalTemp
+                int(lines[3].split()[1]),#RampingStatus
+                [lines[4].split()[1],lines[4].split()[2],lines[4].split()[3]],#PID paratmeters
+                int(lines[5].split()[1]),#Autoscale boolean
+                lines[6]) #time scale
+    except:
+        print('Error Reading Parameters: Edit parameters.txt or redownload it from https://github.com/wendl198/ContactlessProbe.')
+        print('Warning: Data is no longer being recorded.')
+        time.sleep(1)
+        return get_parameters(f) #this will recursively tunnel deeper until the problem is fixed. It will not record data
+    #May be smart to install a better fail safe, but this is probably good enough for most users.
 
-def change_status(new_status):
-    with open(parameter_path) as f:
-        lines = f.readlines()
-    del f
+def change_status(new_status,f):
+    lines = f.readlines()
     lines[3] = lines[3][:-2] + str(new_status) +'\n'
     file1 = open(parameter_path,"w")#write mode
     file1.writelines(lines)
     file1.close()
     del file1, lines
-    #status is an int [0,2]
+    #status is an integer [0,2]
     # No Ramp = 0
     # StartRamp = 1
     # Stop = 2
 
+#######################
+# Cooling Time Prediction
+#######################
+
+def model(x,a,b):
+    return a*np.exp(x*b)+77.3 # a>0, and b<0 when cooling
+parameterlowerbounds1 = np.array([0,-1e1]) # a is the intial temp value - 78 when t=0
+parameterupperbounds1 = np.array([3e2,0])
+pbounds1 = np.array([parameterlowerbounds1,parameterupperbounds1])
+def guess_cool_time(t,T):
+    bestfit = curve_fit(model,t-t[0],T,[T[0]-77.3,-1e-1],bounds=pbounds1) #t must be numpy array
+    bestpars1 = bestfit[0]
+    # parametererr = np.sqrt(np.diag(bestfit[1]))
+    #what to find t_0 in T_final = a * e^(b*t_0) + 77.3
+    #t_o=ln[(T_final-77.3)/a]/b
+    del bestfit, t, T
+    print(bestpars1)
+    return str(np.log((78-77.3)/bestpars1[0])/bestpars1[1])#time til 78K prediction
+
 
 save_path = 'C:\\Users\\mpms\\Desktop\\Contactless Probe\\RawData'
 parameter_path = 'C:\\Users\\mpms\\Desktop\\Contactless Probe\\parameters.txt'
+default_path = 'C:\\Users\\mpms\\Desktop\\Contactless Probe\\default_parameters.txt'
 
 
-#save_path = 'C:/Users/blake/Documents/VSCode/Python/Greven/RawData'
-#parameter_path = 'C:/Users/blake/Documents/VSCode/Python/Greven/parameters.txt'
+# save_path = 'C:/Users/blake/Documents/VSCode/Python/Greven/RawData'
+# parameter_path = 'C:/Users/blake/Documents/VSCode/Python/Greven/parameters.txt'
+# default_path = 'C:/Users/blake/Documents/VSCode/Python/Greven/default_parameters.txt'
 
-avg_num = 4 #this is the number times the lock-in will collect a voltage measurement before the average is reported
+fit_points = 10 # number of data points that are fitted while cooling
+pause_time = .5 #time in sec
 
 #######################
-# Intitiate Save File
+# Open Files
 #######################
-file_input = input("Please type the file name here: ")
-filename = file_input + ".dat"
-completeName = os.path.join(save_path, filename)
-
-file = open(completeName, "a")
-file.write("Time (min)" + "\t" + "T (K)" + "\t" + "Vx (V)" + "\t" + "Vy (V)"+"\n")
-file = completeName
+save_file = open(os.path.join(save_path, input("Please type the file name here: ") + ".dat"), "a")
+save_file.write("Time (min)" + "\t" + "T (K)" + "\t" + "Vx (V)" + "\t" + "Vy (V)"+"\n")
+parameter_file = open(parameter_path, 'r')
+#the idea is to keep the files open, to avoid constant opening and closing. Use .flush() to save data
 
 #######################
 # Open Instuments
@@ -65,7 +88,7 @@ srs = rm.open_resource('GPIB0::13::INSTR')#this is the lock-in
 time.sleep(0.1)
 
 #set intial lakeshore parameters
-parameters = get_parameters()
+parameters = get_parameters(parameter_file)
 ls.write('RAMP 1,0,'+ parameters[0]) #the ramping is intially off
 time.sleep(0.05)
 ls.write('SETP 1,'+ parameters[1])
@@ -100,14 +123,14 @@ del vs
 values = {}
 intitial_time = time.perf_counter()#get intitial time
 
-Tmin = np.inf
-Tmax = -1
-hmin = np.inf
-hmax = -1
-vxmin = np.inf
-vxmax = -2
-vymin = np.inf
-vymax = -2 
+T_min = np.inf
+T_max = -1
+h_min = np.inf
+h_max = -1
+vx_min = np.inf
+vx_max = -2
+vy_min = np.inf
+vy_max = -2 
 
 
 #######################
@@ -119,51 +142,44 @@ while parameters[3] < 2:
     # Update Parameters
     ########################
     del parameters #all local variables are deleted to prevent data pileup in secondary memory that slow processes down
-    parameters = get_parameters()
+    parameters = get_parameters(parameter_file)
 
     values['time'] = (time.perf_counter()-intitial_time)/60 #The time is now in minutes
     values['Temp'] = float(ls.query('KRDG? a')) #temp in K
-    time.sleep(0.05)
-    
-    # values['T_sample'] = float(ls.query('KRDG? b'))
-    # time.sleep(0.05)
     values['heater'] = float(ls.query('HTR? 1')) #this is the percentage of the heater
-    time.sleep(0.05)
+    vs = srs.query('SNAPD?').split(',') #this is [Vx, Vy, Resistance, Theta]
+    values['Vx'] = float(vs[0])
+    values['Vy'] = float(vs[1])
+    del vs
 
-    vx=0
-    vy=0
-    for i in range(avg_num):
-        vs = srs.query('SNAPD?').split(',') #this is [Vx, Vy, Resistance, Theta]
-        vx += float(vs[0])/avg_num
-        vy += float(vs[1])/avg_num
-        del vs
-        time.sleep(0.1)
-    if Tmin > values['Temp']:
-        del Tmin
-        Tmin = values['Temp']
-    elif Tmax < values['Temp']:
-        del Tmax
-        Tmax = values['Temp']
-    if hmin > values['heater']:
-        del hmin
-        hmin = values['heater']
-    elif hmax < values['heater']:
-        del hmax
-        hmax = values['heater']
-    if vxmin > vx:
-        del vxmin
-        vxmin = vx
-    elif vxmax < vx:
-        del vxmax
-        vxmax = vx
-    if vymin > vy:
-        del vymin
-        vymin = vy
-    elif vymax < vy:
-        del vymax
-        vymax = vy
-    values['Vx'] = vx
-    values['Vy'] = vy
+    # update y axis limits
+    if T_min > values['Temp']:
+        del T_min
+        T_min = values['Temp']
+    elif T_max < values['Temp']:
+        del T_max
+        T_max = values['Temp']
+
+    if h_min > values['heater']:
+        del h_min
+        h_min = values['heater']
+    elif h_max < values['heater']:
+        del h_max
+        h_max = values['heater']
+
+    if vx_min > values['Vx']:
+        del vx_min
+        vx_min = values['Vx']
+    elif vx_max < values['Vx']:
+        del vx_max
+        vx_max = values['Vx']
+
+    if vy_min > values['Vy']:
+        del vy_min
+        vy_min = values['Vy']
+    elif vy_max < values['Vy']:
+        del vy_max
+        vy_max = values['Vy']
 
     ########################
     # Update Ramping Status
@@ -174,13 +190,21 @@ while parameters[3] < 2:
         time.sleep(0.05)
         ls.write('SETP 1,'+ parameters[1])# intializes temperature for ramping
 
+        #next part is optional
+        if values['Temp'] > 78:
+            t = p1.get_xdata()
+            if len(t)>fit_points+1:
+                ax.legend().set_visible(True)
+                ax.legend(['78k in ' + guess_cool_time(t[-fit_points:],p1.get_ydata()[-fit_points:]) + ' mins'])
+            del t
+
     elif parameters[3] == 1: #ramp mode
+        ax.legend().set_visible(False)
         ls.write('RAMP 1,1,'+ parameters[0])
         time.sleep(0.05)
         ls.write('SETP 1,'+ parameters[1])#this sets the setpoint to the final temp
         time.sleep(0.05)
         ls.write('PID 1,'+ parameters[4][0]+','+ parameters[4][1]+',' + parameters[4][2])#this sets the setpoint to the final temp
-    time.sleep(0.05)
     
     #######################
     # Plotting
@@ -221,32 +245,30 @@ while parameters[3] < 2:
             bx.set_xlim(left = 0, right = values['time'])
             cx.set_xlim(left = 0, right = values['time'])
             dx.set_xlim(left = 0, right = values['time'])
-    warnings.filterwarnings("ignore")
-    ax.set_ylim(bottom = Tmin, top = Tmax)
-    bx.set_ylim(bottom = hmin, top = hmax)
-    cx.set_ylim(bottom = 1000*vxmin, top = 1000*vxmax)
-    dx.set_ylim(bottom = 1000*vymin, top = 1000*vymax)
+    warnings.filterwarnings("ignore") #often the bounds of the heater are [0,0], this shuts up the warning
+    ax.set_ylim(bottom = T_min, top = T_max)
+    bx.set_ylim(bottom = h_min, top = h_max)
+    cx.set_ylim(bottom = 1000*vx_min, top = 1000*vx_max)
+    dx.set_ylim(bottom = 1000*vy_min, top = 1000*vy_max)
 
     ax.set_title('CurrTemp ='+str(values['Temp']),fontsize = 12)
     bx.set_title('Setpoint ='+str(ls.query('SETP? 1'))[1:6],fontsize = 12)
    
-    plt.pause(0.2) #this displays the graph
+    plt.pause(pause_time) #this displays the graph
 
     #######################
     # Save Data
     #######################
 
-    file = open(completeName, "a")
-    file.write(str(values['time']) + "\t" +  str(values['Temp']) + "\t" + str(values['Vx']) + "\t" + str(values['Vy'])+"\n")
-    file.flush()
-    file.close()
+    save_file.write(str(values['time']) + "\t" +  str(values['Temp']) + "\t" + str(values['Vx']) + "\t" + str(values['Vy'])+"\n")
+    save_file.flush()#this will save the data without closing the file
 
     #check if final temp is reached
     if float(parameters[2]) <= values['Temp']:
-        change_status(0) #stop ramping, but still collect data
+        change_status(0,parameter_file) #stop ramping, but still collect data
 
     del values['time'],values['Temp'],values['Vx'],values['Vy'],values['heater']#,values['T_sample'],values['R'],values['Th'], values['V_total']
-    del vx, vy#, vr, vt
+    del values['Vx'], values['Vy']#, vr, vt
 
 
 #######################
@@ -255,7 +277,17 @@ while parameters[3] < 2:
 ls.write('RAMP 1,0,'+ parameters[0])#turn off ramping
 time.sleep(0.05)
 ls.write('SETP 1,'+ parameters[1]) #set temp back to start
-change_status(0)
 time.sleep(0.05)
 ls.close()
 srs.close()
+
+#######################
+# Reset Parameters
+#######################
+save_file.close()
+parameter_file.close()
+with open(default_path) as f:
+    lines = f.readlines()
+file1 = open(parameter_path,"w")#write mode
+file1.writelines(lines)
+file1.close()
