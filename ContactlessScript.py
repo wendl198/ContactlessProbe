c63605915f87bb8bcb0de04bf99a117536e65169
wendl198 <124744550+wendl198@users.noreply.github.com>
2023-03-10 13:01:04 -0600

    Add files via upload

--- ContactlessScript.py
+++ ContactlessScript.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import pyvisa
 import time
 import os
-import warnings
 from scipy.optimize import curve_fit
 
 def get_parameters(f):
@@ -21,7 +20,7 @@ def get_parameters(f):
                 lines[6]) #time scale
     except:
         print('Error Reading Parameters: Edit parameters.txt or redownload it from https://github.com/wendl198/ContactlessProbe.')
-        print('Warning: Data is no longer being recorded.')
+        print('Warning: Data is not currently being recorded.')
         time.sleep(1)
         return get_parameters(f) #this will recursively tunnel deeper until the problem is fixed. It will not record data
     #May be smart to install a better fail safe, but this is probably good enough for most users.
@@ -33,7 +32,6 @@ def change_status(new_status,f):
     file1 = open(parameter_path,"w")#write mode
     file1.writelines(lines)
     file1.close()
-    del file1, lines
     #status is an integer [0,2]
     # No Ramp = 0
     # StartRamp = 1
@@ -43,20 +41,17 @@ def change_status(new_status,f):
 # Cooling Time Prediction
 #######################
 
-def model(x,a,b):
-    return a*np.exp(x*b)+77.3 # a>0, and b<0 when cooling
-parameterlowerbounds1 = np.array([0,-1e1]) # a is the intial temp value - 78 when t=0
-parameterupperbounds1 = np.array([3e2,0])
+def model(x,a,b,c):
+    return a*np.exp(x*b)+c # a>0, and b<0 when cooling
+parameterlowerbounds1 = np.array([0,-1e1,77]) # a is the intial temp value - 78 when t=0
+parameterupperbounds1 = np.array([3e2,0,100])
 pbounds1 = np.array([parameterlowerbounds1,parameterupperbounds1])
 def guess_cool_time(t,T):
-    bestfit = curve_fit(model,t-t[0],T,[T[0]-77.3,-1e-1],bounds=pbounds1) #t must be numpy array
+    bestfit = curve_fit(model,t-t[0],T,[T[0]-78,-1e-1,78],bounds=pbounds1) #t must be numpy array
     bestpars1 = bestfit[0]
-    # parametererr = np.sqrt(np.diag(bestfit[1]))
-    #what to find t_0 in T_final = a * e^(b*t_0) + 77.3
+    #want to find t_0 in T_final = a * e^(b*t_0) + 77.3
     #t_o=ln[(T_final-77.3)/a]/b
-    del bestfit, t, T
-    #print(bestpars1)
-    return str(np.log((78-77.3)/bestpars1[0])/bestpars1[1])#time til 78K prediction
+    return np.log((78-bestpars1[2])/bestpars1[0])/bestpars1[1]#time til 78K prediction
 
 
 save_path = 'C:\\Users\\mpms\\Desktop\\Contactless Probe\\RawData'
@@ -68,8 +63,10 @@ default_path = 'C:\\Users\\mpms\\Desktop\\Contactless Probe\\default_parameters.
 # parameter_path = 'C:/Users/blake/Documents/VSCode/Python/Greven/parameters.txt'
 # default_path = 'C:/Users/blake/Documents/VSCode/Python/Greven/default_parameters.txt'
 
-fit_points = 10 # number of data points that are fitted while cooling
-pause_time = .5 #time in sec
+# adjustable parameters
+
+fit_points = 15 # number of data points that are fitted while cooling
+pause_time = .7 #time in sec
 
 #######################
 # Open Files
@@ -77,7 +74,7 @@ pause_time = .5 #time in sec
 save_file = open(os.path.join(save_path, input("Please type the file name here: ") + ".dat"), "a")
 save_file.write("Time (min)" + "\t" + "T (K)" + "\t" + "Vx (V)" + "\t" + "Vy (V)"+"\n")
 parameter_file = open(parameter_path, 'r')
-#the idea is to keep the files open, to avoid constant opening and closing. Use .flush() to save data
+#the idea is to keep the files open and avoid constant opening and closing. Use file.flush() to save data
 
 #######################
 # Open Instuments
@@ -93,7 +90,9 @@ time.sleep(0.1)
 parameters = get_parameters(parameter_file)
 ls.write('RAMP 1,0,'+ parameters[0]) #the ramping is intially off
 time.sleep(0.05)
-ls.write('SETP 1,'+ parameters[1])
+ls.write('SETP 1,'+ parameters[1]) #this is the starting temp for the ramp
+time.sleep(0.05)
+ls.write('Range 1,0') #this turns the heater off
 
 #######################
 # Initialize Plots
@@ -120,20 +119,9 @@ p2, = bx.plot(0,float(ls.query('HTR? 1')),'ro-')
 vs = srs.query('SNAPD?').split(',')
 p3, = cx.plot(0,1000*float(vs[0]),'bo-') #plot in mV
 p4,= dx.plot(0,1000*float(vs[1]),'go-')
-del vs
 
 values = {}
 intitial_time = time.perf_counter()#get intitial time
-
-T_min = np.inf
-T_max = -1
-h_min = np.inf
-h_max = -1
-vx_min = np.inf
-vx_max = -2
-vy_min = np.inf
-vy_max = -2 
-
 
 #######################
 # Main Loop
@@ -143,7 +131,6 @@ while parameters[3] < 2:
     ########################
     # Update Parameters
     ########################
-    del parameters #all local variables are deleted to prevent data pileup in secondary memory that slow processes down
     parameters = get_parameters(parameter_file)
 
     values['time'] = (time.perf_counter()-intitial_time)/60 #The time is now in minutes
@@ -152,36 +139,6 @@ while parameters[3] < 2:
     vs = srs.query('SNAPD?').split(',') #this is [Vx, Vy, Resistance, Theta]
     values['Vx'] = float(vs[0])
     values['Vy'] = float(vs[1])
-    del vs
-
-    # update y axis limits
-    if T_min > values['Temp']:
-        del T_min
-        T_min = values['Temp']
-    elif T_max < values['Temp']:
-        del T_max
-        T_max = values['Temp']
-
-    if h_min > values['heater']:
-        del h_min
-        h_min = values['heater']
-    elif h_max < values['heater']:
-        del h_max
-        h_max = values['heater']
-
-    if vx_min > values['Vx']:
-        del vx_min
-        vx_min = values['Vx']
-    elif vx_max < values['Vx']:
-        del vx_max
-        vx_max = values['Vx']
-
-    if vy_min > values['Vy']:
-        del vy_min
-        vy_min = values['Vy']
-    elif vy_max < values['Vy']:
-        del vy_max
-        vy_max = values['Vy']
 
     ########################
     # Update Ramping Status
@@ -191,14 +148,14 @@ while parameters[3] < 2:
         ls.write('RAMP 1,0,'+ parameters[0])# Turns off ramping
         time.sleep(0.05)
         ls.write('SETP 1,'+ parameters[1])# intializes temperature for ramping
+        time.sleep(0.05)
+        ls.write('Range 1,0') #this turns the heater off
 
         #next part is optional
-        if values['Temp'] > 780:
+        if values['Temp'] > 78:
             t = p1.get_xdata()
             if len(t)>fit_points+1:
-                ax.legend().set_visible(True)
-                ax.legend(['78k in ' + guess_cool_time(t[-fit_points:],p1.get_ydata()[-fit_points:]) + ' mins'])
-            del t
+                ax.text(0.05, 0.95, '78k in ' + str(round(guess_cool_time(t[-fit_points:],p1.get_ydata()[-fit_points:]),1)) + ' mins', transform=ax.transAxes,fontsize=8)
 
     elif parameters[3] == 1: #ramp mode
         #ax.legend().set_visible(False)
@@ -207,56 +164,79 @@ while parameters[3] < 2:
         ls.write('SETP 1,'+ parameters[2])#this sets the setpoint to the final temp
         time.sleep(0.05)
         ls.write('PID 1,'+ parameters[4][0]+','+ parameters[4][1]+',' + parameters[4][2])#this sets the setpoint to the final temp
+        time.sleep(0.05)
+        ls.write('Range 1,1') #this turns the heater to low
+        for txt in fig.texts:
+            txt.set_visible(False) #clears textbox with cooltime prediction
     
     #######################
     # Plotting
     #######################
 
-    # ax.plot(values['time'],values['Temp'],'ko--')
-    # bx.plot(values['time'],values['heater'],'ro--')
-    # cx.plot(values['time'],1000*values['Vx'],'bo--') #plot in mV
-    # dx.plot(values['time'],1000*values['Vy'],'go--')
+    # update data
+    times = np.append(p1.get_xdata(),values['time'])
+    y1 = np.append(p1.get_ydata(),values['Temp'])
+    y2 = np.append(p2.get_ydata(),values['heater'])
+    y3 = np.append(p3.get_ydata(),1000*values['Vx']) # plot the voltages in mV
+    y4 = np.append(p4.get_ydata(),1000*values['Vy'])
 
-    p1.set_xdata(np.append(p1.get_xdata(),values['time']))
-    p1.set_ydata(np.append(p1.get_ydata(),values['Temp']))
-    p2.set_xdata(np.append(p2.get_xdata(),values['time']))
-    p2.set_ydata(np.append(p2.get_ydata(),values['heater']))
-    p3.set_xdata(np.append(p3.get_xdata(),values['time']))
-    p3.set_ydata(np.append(p3.get_ydata(),1000*values['Vx']))
-    p4.set_xdata(np.append(p4.get_xdata(),values['time']))
-    p4.set_ydata(np.append(p4.get_ydata(),1000*values['Vy']))
+    p1.set_xdata(times)
+    p2.set_xdata(times)
+    p3.set_xdata(times)
+    p4.set_xdata(times)
+    p1.set_ydata(y1)
+    p2.set_ydata(y2)
+    p3.set_ydata(y3)
+    p4.set_ydata(y4)
+
+    #update limits
 
     if parameters[5]:
         ax.set_xlim(left = 0, right = values['time'])
         bx.set_xlim(left = 0, right = values['time'])
         cx.set_xlim(left = 0, right = values['time'])
         dx.set_xlim(left = 0, right = values['time'])
+        ax.set_ylim(bottom = y1.min(), top = y1.max())
+        bx.set_ylim(bottom = y2.min(), top = y2.max())
+        cx.set_ylim(bottom = y3.min(), top = y3.max())
+        dx.set_ylim(bottom = y4.min(), top = y4.max())
     else:
         if len(parameters[6]) == 2:
-            ax.set_xlim(left = float(parameters[6][0]),right = float(parameters[6][1]))
-            bx.set_xlim(left = float(parameters[6][0]),right = float(parameters[6][1]))
-            cx.set_xlim(left = float(parameters[6][0]),right = float(parameters[6][1]))
-            dx.set_xlim(left = float(parameters[6][0]),right = float(parameters[6][1]))
+            t0 = float(parameters[6][0])
+            t1 = float(parameters[6][1])
+            ind0 = np.argsort(t0)
+            ind1 = np.argsort(t1)
+            ax.set_xlim(left = t0,right = t1)
+            bx.set_xlim(left = t0,right = t1)
+            cx.set_xlim(left = t0,right = t1)
+            dx.set_xlim(left = t0,right = t1)
+            ax.set_ylim(bottom = y1[ind0:ind1].min(), top = y1[ind0:ind1].max())
+            bx.set_ylim(bottom = y2[ind0:ind1].min(), top = y2[ind0:ind1].max())
+            cx.set_ylim(bottom = y3[ind0:ind1].min(), top = y3[ind0:ind1].max())
+            dx.set_ylim(bottom = y4[ind0:ind1].min(), top = y4[ind0:ind1].max())
         elif len(parameters[6]) == 1:
-            ax.set_xlim(left = float(parameters[6][0]),right = values['time'])
-            bx.set_xlim(left = float(parameters[6][0]),right = values['time'])
-            cx.set_xlim(left = float(parameters[6][0]),right = values['time'])
-            dx.set_xlim(left = float(parameters[6][0]),right = values['time'])
+            t0 = float(parameters[6][0])
+            ind0 = np.argsort(t0)
+            ax.set_xlim(left = t0,right = values['time'])
+            bx.set_xlim(left = t0,right = values['time'])
+            cx.set_xlim(left = t0,right = values['time'])
+            dx.set_xlim(left = t0,right = values['time'])
+            ax.set_ylim(bottom = y1[ind0:].min(), top = y1[ind0:].max())
+            bx.set_ylim(bottom = y2[ind0:].min(), top = y2[ind0:].max())
+            cx.set_ylim(bottom = y3[ind0:].min(), top = y3[ind0:].max())
+            dx.set_ylim(bottom = y4[ind0:].min(), top = y4[ind0:].max())
         else:
             ax.set_xlim(left = 0, right = values['time'])
             bx.set_xlim(left = 0, right = values['time'])
             cx.set_xlim(left = 0, right = values['time'])
             dx.set_xlim(left = 0, right = values['time'])
-    warnings.filterwarnings("ignore") #often the bounds of the heater are [0,0], this shuts up the warning
-    ax.set_ylim(bottom = T_min, top = T_max)
-    bx.set_ylim(bottom = h_min, top = h_max)
-    cx.set_ylim(bottom = 1000*vx_min, top = 1000*vx_max)
-    dx.set_ylim(bottom = 1000*vy_min, top = 1000*vy_max)
+            ax.set_ylim(bottom = y1.min(), top = y1.max())
+            bx.set_ylim(bottom = y2.min(), top = y2.max())
+            cx.set_ylim(bottom = y3.min(), top = y3.max())
+            dx.set_ylim(bottom = y4.min(), top = y4.max())
 
     ax.set_title('CurrTemp ='+str(values['Temp']),fontsize = 12)
     bx.set_title('Setpoint ='+str(ls.query('SETP? 1'))[1:6],fontsize = 12)
-   
-    plt.pause(pause_time) #this displays the graph
 
     #######################
     # Save Data
@@ -265,11 +245,12 @@ while parameters[3] < 2:
     save_file.write(str(values['time']) + "\t" +  str(values['Temp']) + "\t" + str(values['Vx']) + "\t" + str(values['Vy'])+"\n")
     save_file.flush()#this will save the data without closing the file
 
+
+    plt.pause(pause_time) #this displays the graph
+
     #check if final temp is reached
     if float(parameters[2]) <= values['Temp']:
         change_status(0,parameter_file) #stop ramping, but still collect data
-
-    del values['time'],values['Temp'],values['Vx'],values['Vy'],values['heater']#,values['T_sample'],values['R'],values['Th'], values['V_total']
 
 
 #######################
@@ -278,6 +259,8 @@ while parameters[3] < 2:
 ls.write('RAMP 1,0,'+ parameters[0])#turn off ramping
 time.sleep(0.05)
 ls.write('SETP 1,'+ parameters[1]) #set temp back to start
+time.sleep(0.05)
+ls.write('Range 1,0') #this turns the heater off
 time.sleep(0.05)
 ls.close()
 srs.close()
