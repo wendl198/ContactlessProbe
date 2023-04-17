ff03cd64c0cdf7a969e3992c8d999dec9ff51021
wendl198 <124744550+wendl198@users.noreply.github.com>
2023-04-17 18:36:25 -0500

    Add files via upload
    First Iteration of phase v temp plotting

--- DataAcquisition/ContactlessScript.py
+++ DataAcquisition/ContactlessScript.py
@@ -17,14 +17,15 @@ def get_parameters(f):
         f.seek(0) #resets pointer to top of the file
         lines = f.readlines()
         lines[6] = lines[6].split()
-        lines[6].pop(0) #this removes the word, but still returns a list
+        lines[6].pop(0) #this removes the word, but still returns a list if empty
         return (lines[0].split()[1],#RampRate
                 lines[1].split()[1],#StartTemp
                 lines[2].split()[1],#FinalTemp
                 int(lines[3].split()[1]),#RampingStatus
                 [lines[4].split()[1],lines[4].split()[2],lines[4].split()[3]],#PID paratmeters
                 int(lines[5].split()[1]),#Autoscale boolean
-                lines[6]) #time scale
+                lines[6],#time scale
+                lines[7].split()[1:2])# voltage offset values [Vx,Vy] in mV
     except:
         print('Error Reading Parameters: Edit parameters.txt or redownload it from https://github.com/wendl198/ContactlessProbe.')
         print('Warning: Data is not currently being recorded.')
@@ -43,34 +44,6 @@ def change_status(new_status,f):
     # No Ramp = 0
     # StartRamp = 1
     # Stop = 2
-
-def find_nearest_ind(bounds,times):
-    output = []
-    l = len(times)
-    # t is an ordered list, so we can do a binary search to find the nearest ind in O(log(l)) time
-    for t in bounds:
-        i = l//2
-        divisor = 4
-        try:
-            while times[i] > t or times[i+1] < t: #stops when t is between times[i] and times[i+1]
-                if times[i] > t:
-                    i -= l//divisor+1 #this is simply the ceiling operation (close enough)
-                else:
-                    i += l//divisor+1
-                divisor*=2
-            if t-times[i] >= .5:
-                output.append(i+1)
-            else:
-                output.append(i)
-        except:# this is meant to be boundary condiitons: if the t is not in the list, the lowest or highest index will be returned 
-            if i < 0:
-                output.append(0)
-            elif i + 1 >= l:
-                output.append(l-1)
-            else:
-                print('Error in time scaling.\nUnsure why i = ',i)
-                output.append(0) 
-    return output
 
 #paths
 save_path = 'C:\\Users\\mpms\\Desktop\\Contactless Probe\\RawData'
@@ -120,15 +93,17 @@ dx = fig.add_subplot(2, 2, 4)
 ax.set_xlabel('Time (min)')
 ax.set_ylabel('Temperature (K)')
 bx.set_xlabel('Time (min)')
-bx.set_ylabel('Heater Percent (%)')
+bx.set_ylabel('Phase of Voltage (rad)')
 cx.set_xlabel('Time (min)')
 cx.set_ylabel('Vx (mV)')
 dx.set_xlabel('Time (min)')
 dx.set_ylabel('Vy (mV)')
 
-p1, = ax.plot(0,float(ls.query('KRDG? a')),'ko-')
-p2, = bx.plot(0,float(ls.query('HTR? 1')),'ro-')
+T0 = float(ls.query('KRDG? a'))
+p1, = ax.plot(0,T0,'ko-')
 vs = srs.query('SNAPD?').split(',')
+# p2, = bx.plot(0,float(ls.query('HTR? 1')),'ro-')
+p2, = bx.plot(T0,np.angle(float(vs[0])-parameters[7][0]+(float(vs[1])-parameters[7][1])*1j),'ro-')
 p3, = cx.plot(0,1000*float(vs[0]),'bo-') #plot in mV
 p4,= dx.plot(0,1000*float(vs[1]),'go-')
 
@@ -147,7 +122,6 @@ while parameters[3] < 2:
 
     values['time'] = (time.perf_counter()-intitial_time)/60 #The time is now in minutes
     values['Temp'] = float(ls.query('KRDG? a')) #temp in K
-    values['heater'] = float(ls.query('HTR? 1')) #this is the percentage of the heater
     vs = srs.query('SNAPD?').split(',') #this is [Vx, Vy, Resistance, Theta]
     values['Vx'] = float(vs[0])
     values['Vy'] = float(vs[1])
@@ -183,24 +157,25 @@ while parameters[3] < 2:
     # update data
     times = np.append(p1.get_xdata(),values['time'])
     y1 = np.append(p1.get_ydata(),values['Temp'])
-    y2 = np.append(p2.get_ydata(),values['heater'])
     y3 = np.append(p3.get_ydata(),1000*values['Vx']) # plot the voltages in mV
     y4 = np.append(p4.get_ydata(),1000*values['Vy'])
+    y2 = np.angle(y3-parameters[7][0]+(y4-parameters[7][1])*1j)
 
     p1.set_xdata(times)
-    p2.set_xdata(times)
     p3.set_xdata(times)
     p4.set_xdata(times)
     p1.set_ydata(y1)
-    p2.set_ydata(y2)
     p3.set_ydata(y3)
     p4.set_ydata(y4)
 
-    #update limits
+    #update limits 
+    # Note p2 is special because temperature on x axis
 
     if parameters[5]:
+        p2.set_xdata(y1)
+        p2.set_ydata(y2)
         ax.set_xlim(left = 0, right = values['time'])
-        bx.set_xlim(left = 0, right = values['time'])
+        bx.set_xlim(left = y1.min(), right = y1.max())
         cx.set_xlim(left = 0, right = values['time'])
         dx.set_xlim(left = 0, right = values['time'])
         ax.set_ylim(bottom = y1.min(), top = y1.max())
@@ -211,42 +186,34 @@ while parameters[3] < 2:
         if len(parameters[6]) == 2:
             t0 = float(parameters[6][0])
             t1 = float(parameters[6][1])
-            inds = find_nearest_ind([t0,t1],times)
-            ind0 = inds[0]
-            ind1 = inds[1]
-            if ind0 == ind1:
-                if ind0 == 0:
-                    ind1 = 1
-                else:
-                    ind0 -= 1
+            inds = np.logical_and(times >= t0, times <= t1)
+            p2.set_xdata(y1[inds])
+            p2.set_ydata(y2[inds])
             ax.set_xlim(left = t0,right = t1)
-            bx.set_xlim(left = t0,right = t1)
+            bx.set_xlim(left = y1[inds].min(),right = y1[inds].max())
             cx.set_xlim(left = t0,right = t1)
             dx.set_xlim(left = t0,right = t1)
-            ax.set_ylim(bottom = y1[ind0:ind1 + 1].min(), top = y1[ind0:ind1 + 1].max())
-            bx.set_ylim(bottom = y2[ind0:ind1 + 1].min(), top = y2[ind0:ind1 + 1].max())
-            cx.set_ylim(bottom = y3[ind0:ind1 + 1].min(), top = y3[ind0:ind1 + 1].max())
-            dx.set_ylim(bottom = y4[ind0:ind1 + 1].min(), top = y4[ind0:ind1 + 1].max())
         elif len(parameters[6]) == 1:
             t0 = float(parameters[6][0])
-            ind0 = find_nearest_ind([t0],times)[0]
+            inds = np.logical_not(times<t0)
+            p2.set_xdata(y1[inds])
+            p2.set_ydata(y2[inds])
             ax.set_xlim(left = t0,right = values['time'])
-            bx.set_xlim(left = t0,right = values['time'])
+            bx.set_xlim(left = y1[inds].min(),right = y1[inds].max())
             cx.set_xlim(left = t0,right = values['time'])
             dx.set_xlim(left = t0,right = values['time'])
-            ax.set_ylim(bottom = y1[ind0:].min(), top = y1[ind0:].max())
-            bx.set_ylim(bottom = y2[ind0:].min(), top = y2[ind0:].max())
-            cx.set_ylim(bottom = y3[ind0:].min(), top = y3[ind0:].max())
-            dx.set_ylim(bottom = y4[ind0:].min(), top = y4[ind0:].max())
         else:
+            inds = np.logical_not(times<0)
+            p2.set_xdata(y1)
+            p2.set_ydata(y2)
             ax.set_xlim(left = 0, right = values['time'])
-            bx.set_xlim(left = 0, right = values['time'])
+            bx.set_xlim(left = y1.min(), right = y1.max())
             cx.set_xlim(left = 0, right = values['time'])
             dx.set_xlim(left = 0, right = values['time'])
-            ax.set_ylim(bottom = y1.min(), top = y1.max())
-            bx.set_ylim(bottom = y2.min(), top = y2.max())
-            cx.set_ylim(bottom = y3.min(), top = y3.max())
-            dx.set_ylim(bottom = y4.min(), top = y4.max())
+        ax.set_ylim(bottom = y1[inds].min(), top = y1[inds].max())
+        bx.set_ylim(bottom = y2[inds].min(), top = y2[inds].max())
+        cx.set_ylim(bottom = y3[inds].min(), top = y3[inds].max())
+        dx.set_ylim(bottom = y4[inds].min(), top = y4[inds].max())
 
     
 
