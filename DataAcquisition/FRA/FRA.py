56b9e1ba6524830685f8d42f542a129397a685de
wendl198 <124744550+wendl198@users.noreply.github.com>
2023-07-14 14:32:32 -0500

    Add files via upload

--- DataAcquisition/FRA/FRA.py
+++ DataAcquisition/FRA/FRA.py
@@ -13,7 +13,23 @@ save_file.write("Time (min)" + "\t" + "T (K)" + "\t" + 'Freq (kHz)' + "\t"+ "Vx 
 set_temp = 92
 freqs = np.array([100.003,250.003,400.003,671.111,800.003,1000.03,1200.03,1500.03,1800.03,2000.03,2500.03,3000.03,4000])
 trials = 3
-
+sens_dict = {1000:0,
+            500:1,
+            200:2,
+            100:3,
+            50:4,
+            20:5,
+            10:6,
+            5:7,
+            2:8,
+            1:9,
+            .5:10,
+            .2:11}
+input_range_dict = {1000:0,
+                    300:1,
+                    100:2,
+                    30:3,
+                    10:4}
 rm = pyvisa.ResourceManager()
 ls = rm.open_resource('GPIB0::16::INSTR')#this is the lake shore temp controller
 time.sleep(0.1)
@@ -33,6 +49,11 @@ for i in range(trials):
             srs.query('FREQ '+ str(freq) + ' KHZ')
         except:
             pass
+        vs = srs.query('SNAPD?').split(',') #this is [Vx, Vy, Voltage Magnitude, Theta]
+        j = sens_dict[sens_dict.keys()[np.logical_not(sens_dict.keys()<vs[2])][0]]
+        k = sens_dict[sens_dict.keys()[np.logical_not(input_range_dict.keys()<vs[2])][0]]
+        vs = srs.query('IRNG '+str(k))
+        vs = srs.query('SCAL '+str(j))
         time.sleep(3)
         values['time'] = (time.perf_counter()-intitial_time)/60 #The time is now in minutes
         values['Temp'] = float(ls.query('KRDG? a')) #temp in K
