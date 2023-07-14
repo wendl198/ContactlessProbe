ddcf13d588f115f26b2bba7bda2b9d3e2f464ab8
wendl198 <124744550+wendl198@users.noreply.github.com>
2023-07-14 13:46:10 -0500

    Update StartFRA.bat

--- DataAcquisition/FRA/StartFRA.bat
+++ DataAcquisition/FRA/StartFRA.bat
@@ -1,2 +1,3 @@
+start notepad "C:\Users\mpms\Desktop\Contactless Probe\parameters.txt"
 "C:\Users\mpms\Desktop\Contactless Probe\Contactless venv\Scripts\python.exe" "C:\Users\mpms\Desktop\Contactless Probe\FRA.py"
 PAUSE
