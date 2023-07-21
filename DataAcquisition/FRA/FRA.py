f37fb7f12eab8e5568845dd248079c6b906765a8
wendl198 <124744550+wendl198@users.noreply.github.com>
2023-07-21 15:39:30 -0500

    Add files via upload

--- DataAcquisition/FRA/FRA.py
+++ DataAcquisition/FRA/FRA.py
@@ -11,7 +11,7 @@ save_file = open(os.path.join(save_path, input("Please type the file name here: 
 save_file.write("Time (min)" + "\t" + "T (K)" + "\t" + 'Freq (kHz)' + "\t"+ "Vx (V)" + "\t" + "Vy (V)"+"\n")
 
 set_temp = 92
-freqs = np.array([100.003,250.003,400.003,671.111,800.003,1000.03,1200.03,1500.03,1800.03,2000.03,2500.03,3000.03,4000])
+freqs = np.array([100.003,250.003,400.003,500.003,600.003,671.111,800.003,900.003,1000.03,1250.03,1500.03,1800.03,2000.03,2250.03,2500.03,2750.03,3000.03,3500.03,4000])
 trials = 3
 sens_dict = {1000:0,
             500:1,
@@ -40,9 +40,11 @@ ls = rm.open_resource('GPIB0::16::INSTR')#this is the lake shore temp controller
 time.sleep(0.1)
 srs = rm.open_resource('GPIB0::13::INSTR')#this is the lock-in
 time.sleep(0.1)
-ls.write('RAMP 1,1,'+ str(set_temp))
+ls.write('Range 1,1')
 time.sleep(0.1)
 ls.write('SETP 1,'+ str(set_temp))
+
+input('Temp Stable??')
 
 values = {}
 intitial_time = time.perf_counter()#get intitial time
@@ -57,9 +59,15 @@ for i in range(trials):
         R = float(srs.query('SNAPD?').split(',')[2])*1000 #this is the Voltage Magnitude in mV
         j = sens_dict[sens_keys[np.logical_not(sens_keys<R)][0]]
         k = input_range_dict[input_range_keys[np.logical_not(input_range_keys<R)][0]]
-        vs = srs.query('IRNG '+str(k))
-        vs = srs.query('SCAL '+str(j))
-        time.sleep(3)
+        try:
+            vs = srs.query('IRNG '+str(k))
+        except:
+            pass
+        try:
+            vs = srs.query('SCAL '+str(j))
+        except:
+            pass
+        time.sleep(1)
         values['time'] = (time.perf_counter()-intitial_time)/60 #The time is now in minutes
         values['Temp'] = float(ls.query('KRDG? a')) #temp in K
         vs = srs.query('SNAPD?').split(',') #this is [Vx, Vy, Voltage Magnitude, Theta]
