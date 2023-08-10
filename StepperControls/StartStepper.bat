fa04dfb831b0e7aa35c0b108ff8ea2013a4ea74b
wendl198 <124744550+wendl198@users.noreply.github.com>
2023-08-10 11:30:35 -0500

    Add files via upload

---
+++ StepperControls/StartStepper.bat
@@ -0,0 +1,3 @@
+start notepad "C:\Users\Contactless\Desktop\Stepper\StepperParameters.txt"
+"C:\Users\Contactless\Desktop\Stepper\StepperVenv\Scripts\python.exe" "C:\Users\Contactless\Desktop\Stepper\GrowthStepper.py"
+PAUSE
