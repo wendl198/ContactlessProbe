52bbc515036720f755408749a41a31af40af2d4c
wendl198 <124744550+wendl198@users.noreply.github.com>
2023-08-10 10:17:50 -0500

    Add files via upload

--- StepperControls/GrowthStepper.py
+++ StepperControls/GrowthStepper.py
@@ -1,58 +1,77 @@
 from Phidget22.Phidget import *
-from Phidget22.Devices.Log import *
-from Phidget22.LogLevel import *
 from Phidget22.Devices.Stepper import *
 from Phidget22.Devices.DigitalInput import *
 import time
 
+def get_parameters(f):
+    try:
+        f.seek(0) #resets pointer to top of the file
+        lines = f.readlines()
+        return (lines[0].split()[1],#MaxSpeed
+                lines[1].split()[1])#SomethingElse
+    except:
+        print('Error Reading Parameters: Edit parameters.txt or redownload it from https://github.com/wendl198/ContactlessProbe.')
+        time.sleep(1)
+        return get_parameters(f) #this will recursively tunnel deeper until the problem is fixed. It will not record data
+    #May be smart to install a better fail safe, but this is probably good enough for most users.
+
+
+StepsPerRev = 360/1.8*(204687/2057)*16 #200steps/rev*gear ratio*16microsteps
+timeout = 10000
+
+parameter_path = 'C:\\Users\\Contactless\\Desktop\\Stepper\\StepperParameters.txt'
+parameter_file = open(parameter_path, 'r')
+        
 #Declare any event handlers here. These will be called every time the associated event occurs.
 
 def onDigitalInput1_StateChange(self, state):
-	print("State [1]: " + str(state))
+    print("State [1]: " + str(state))
 
 def onDigitalInput2_StateChange(self, state):
-	print("State [2]: " + str(state))
+    print("State [2]: " + str(state))
 
 def onDigitalInput3_StateChange(self, state):
-	print("State [3]: " + str(state))
+    print("State [3]: " + str(state))
 
 def main():
-	Log.enable(LogLevel.PHIDGET_LOG_INFO, "phidgetlog.log")
-	#Create your Phidget channels
-	stepper0 = Stepper()
-	digitalInput1 = DigitalInput()
-	digitalInput2 = DigitalInput()
-	digitalInput3 = DigitalInput()
+    #Create your Phidget channels
+    stepper0 = Stepper()
+    digitalInput1 = DigitalInput()
+    digitalInput2 = DigitalInput()
+    digitalInput3 = DigitalInput()
 
-	#Set addressing parameters to specify which channel to open (if any)
-	digitalInput1.setChannel(1)
-	digitalInput2.setChannel(2)
-	digitalInput3.setChannel(3)
+    #Set addressing parameters to specify which channel to open (if any)
+    digitalInput1.setChannel(1)
+    digitalInput2.setChannel(2)
+    digitalInput3.setChannel(3)
+    #Assign any event handlers you need before calling open so that no events are missed.
+    digitalInput1.setOnStateChangeHandler(onDigitalInput1_StateChange)
+    digitalInput2.setOnStateChangeHandler(onDigitalInput2_StateChange)
+    digitalInput3.setOnStateChangeHandler(onDigitalInput3_StateChange)
 
-	#Assign any event handlers you need before calling open so that no events are missed.
-	digitalInput1.setOnStateChangeHandler(onDigitalInput1_StateChange)
-	digitalInput2.setOnStateChangeHandler(onDigitalInput2_StateChange)
-	digitalInput3.setOnStateChangeHandler(onDigitalInput3_StateChange)
+    #Open your Phidgets and wait for attachment
+    stepper0.openWaitForAttachment(5000)
+    digitalInput1.openWaitForAttachment(5000)
+    digitalInput2.openWaitForAttachment(5000)
+    digitalInput3.openWaitForAttachment(5000)
 
-	#Open your Phidgets and wait for attachment
-	stepper0.openWaitForAttachment(5000)
-	digitalInput1.openWaitForAttachment(5000)
-	digitalInput2.openWaitForAttachment(5000)
-	digitalInput3.openWaitForAttachment(5000)
-
-	#Do stuff with your Phidgets here or in your event handlers.
-	stepper0.setTargetPosition(10000)
-	stepper0.setEngaged(True)
-
-	try:
-		input("Press Enter to Stop\n")
-	except (Exception, KeyboardInterrupt):
-		pass
-
-	#Close your Phidgets once the program is done.
-	stepper0.close()
-	digitalInput1.close()
-	digitalInput2.close()
-	digitalInput3.close()
+    #Do stuff with your Phidgets here or in your event handlers.
+    stepper0.setTargetPosition(int(StepsPerRev))
+    stepper0.setEngaged(True)
+    while stepper0.getTargetPosition() != stepper0.getPosition():
+        speed,_ = get_parameters(parameter_file)
+        stepper0.setVelocityLimit(int(speed))
+        #get_parameters
+    time.sleep(3)
+    stepper0.addPositionOffset(0)
+    stepper0.setTargetPosition(0) #this zeros the position
+    while stepper0.getTargetPosition() != stepper0.getPosition():
+        speed,_ = get_parameters(parameter_file)
+        stepper0.setVelocityLimit(int(speed))
+    #Close your Phidgets once the program is done.
+    stepper0.close()
+    digitalInput1.close()
+    digitalInput2.close()
+    digitalInput3.close()
 
 main()
