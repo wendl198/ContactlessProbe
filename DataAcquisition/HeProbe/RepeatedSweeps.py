212fbd4c9d3870678cac2a5e6e7e98db4398a5d9
wendl198 <124744550+wendl198@users.noreply.github.com>
2024-02-05 16:14:40 -0600

    Add files via upload

--- DataAcquisition/HeProbe/RepeatedSweeps.py
+++ DataAcquisition/HeProbe/RepeatedSweeps.py
@@ -280,6 +280,7 @@ while parameters[6] < 3:#main loop
     while parameters[6] == 1: #ramp mode
         intiate_scan(srs,500,4000,2000,30,False)
         vs = srs.query('SNAPD?').split(',') #this is [Vx, Vy, Vmag, freq]
+        print(vs)
         values['Vx'] = float(vs[0])
         values['Vy'] = float(vs[1])
         values['Vmag'] = float(vs[2])
@@ -374,7 +375,7 @@ while parameters[6] < 3:#main loop
         change_status(2,parameter_file)
         parameters = get_parameters(parameter_file)
         srs.write('SCNENBL 0')
-        time.sleep(.1)
+        print('Switching to Temp Ramp')
 
 
     while parameters[6] == 2: #ramp mode
@@ -398,20 +399,22 @@ while parameters[6] < 3:#main loop
         parameters = get_parameters(parameter_file)
 
         intiate_scan(srs,f_center-parameters[4]/2,f_center+parameters[4]/2,parameters[5],parameters[3],False)
-        vs = srs.query('SNAPD?').split(',') #this is [Vx, Vy, Vmag, freq]
-        values['Vx'] = float(vs[0])
-        values['Vy'] = float(vs[1])
-        values['Vmag'] = float(vs[2])
-        values['freq'] = float(vs[3])
-        p4.set_xdata([values['freq']])
-        p5.set_xdata([values['freq']])
-        p6.set_xdata([values['freq']])
-        p4.set_ydata([values['Vx']])
-        p5.set_ydata([values['Vy']])
-        p6.set_ydata([values['Vmag']])
+# data_capture_command_list = [
+#     'CAPTURECFG 3', #sets capture of all four displayed parameters
+#     'CAPTURERATE 6', #sets capture rate to 1.2kHz for 3-10 ms time constant
+#     'CAPTURESTART ONE, IMM', #start scan immedaitely
+#     'CAPTURESTOP' #stop scan
+#     'CAPTUREPROG?' #get kbyte size of completed capture
+#     'CAPTUREGET? 0 ' + str 
+# ]
+        if sweep_num == 0:
+            srs.write('CAPTURECFG 3')#sets capture of all four displayed parameters
+            srs.write('CAPTURERATE 6')#sets capture rate to 1.2kHz for 3-10 ms time constant
+
 
         sweep_num += 1 #this will help identify sweeps from each other
         srs.write('SCNRUN') #start scan
+        srs.write('CAPTURESTART ONE, IMM')
         while srs.query('SCNSTATE?').strip() == '2':#scanning
             #######################
             # Collect Data
@@ -433,98 +436,101 @@ while parameters[6] < 3:#main loop
             save_file.flush()
 
             plt.pause(3*time_con)
-
+        srs.write('CAPTURESTOP')
+        byte_num = srs.query('CAPTUREPROG?').strip()
+        raw_data = srs.query('CAPTUREGET? 0 ' + byte_num)
         srs.write('SCNENBL 0')
         parameters = get_parameters(parameter_file)
+        print(raw_data)
         #this part can afford to be slower because it is called 100x less
 
         #######################
         # Retrieve Data
         #######################
-        new_data = [[],[],[],[],[],[]]#[time,temp,vx,vy,vmag,freq]
-        with open(save_path, 'r') as file:
-            last_lines = deque(file, maxlen=parameters[3]//(3*time_con))
-        for line in last_lines:
-            data = line.split()
-            if data[-1] == sweep_num:
-                for i, dat in enumerate(data[:-1]):
-                    new_data[i].append(float(dat))
-        new_data= np.array(new_data)
+        # new_data = [[],[],[],[],[],[]]#[time,temp,vx,vy,vmag,freq]
+        # with open(save_path, 'r') as file:
+        #     last_lines = deque(file, maxlen=parameters[3]//(3*time_con))
+        # for line in last_lines:
+        #     data = line.split()
+        #     if data[-1] == sweep_num:
+        #         for i, dat in enumerate(data[:-1]):
+        #             new_data[i].append(float(dat))
+        # new_data= np.array(new_data)
 
 
         #######################
         # fitting
         #######################
 
-        guesses1 = [f_center*1000,30,-.3,.26,0,0]
-        pbounds1 = np.array([[min(new_data[5]),1,-1,-1,-1,-1],[max(new_data[5]),1e4,1,1,1,1]]) # [[Lower bounds],[upper bounds]]
-        bestfit = optimize.curve_fit(full_lorenzian_fit_with_skew,new_data[5],new_data[4]*1000,guesses1, bounds=pbounds1)
-        bestpars = bestfit[0]
+        # guesses1 = [f_center*1000,30,-.3,.26,0,0]
+        # pbounds1 = np.array([[min(new_data[5]),1,-1,-1,-1,-1],[max(new_data[5]),1e4,1,1,1,1]]) # [[Lower bounds],[upper bounds]]
+        # bestfit = optimize.curve_fit(full_lorenzian_fit_with_skew,new_data[5],new_data[4]*1000,guesses1, bounds=pbounds1)
+        # bestpars = bestfit[0]
 
         #######################
         # Plotting
         #######################
 
-        #append time data
-        times = np.append(p1.get_xdata(),new_data[0])
-        y1 = np.append(p1.get_ydata(),new_data[1])
-        p1.set_xdata(times)
-        p1.set_ydata(y1)
+        # #append time data
+        # times = np.append(p1.get_xdata(),new_data[0])
+        # y1 = np.append(p1.get_ydata(),new_data[1])
+        # p1.set_xdata(times)
+        # p1.set_ydata(y1)
 
-        #show voltage for previous scan
-        p4.set_xdata(new_data[5]/1000)
-        p5.set_xdata(new_data[5]/1000)
-        p6.set_xdata(new_data[5]/1000)
-        p4.set_ydata(1000*new_data[2])
-        p5.set_ydata(1000*new_data[3])
-        p6.set_ydata(1000*new_data[4])
+        # #show voltage for previous scan
+        # p4.set_xdata(new_data[5]/1000)
+        # p5.set_xdata(new_data[5]/1000)
+        # p6.set_xdata(new_data[5]/1000)
+        # p4.set_ydata(1000*new_data[2])
+        # p5.set_ydata(1000*new_data[3])
+        # p6.set_ydata(1000*new_data[4])
 
-        #Q factor and res freq data
-        if sweep_num == 1:
-            p2.set_xdata([(T_avg := np.average(new_data[1]))])
-            p3.set_xdata([T_avg])
-            p2.set_ydata([bestpars[0]])
-            p3.set_ydata([bestpars[1]])
-        else:
-            p2.set_xdata(np.append(p2.get_xdata(),(T_avg := np.average(new_data[1]))))
-            p3.set_xdata(np.append(p3.get_xdata(),T_avg))
-            p2.set_ydata(np.append(p2.get_ydata(),bestpars[0]))
-            p3.set_ydata(np.append(p3.get_ydata(),bestpars[1]))
+        # #Q factor and res freq data
+        # if sweep_num == 1:
+        #     p2.set_xdata([(T_avg := np.average(new_data[1]))])
+        #     p3.set_xdata([T_avg])
+        #     p2.set_ydata([bestpars[0]])
+        #     p3.set_ydata([bestpars[1]])
+        # else:
+        #     p2.set_xdata(np.append(p2.get_xdata(),(T_avg := np.average(new_data[1]))))
+        #     p3.set_xdata(np.append(p3.get_xdata(),T_avg))
+        #     p2.set_ydata(np.append(p2.get_ydata(),bestpars[0]))
+        #     p3.set_ydata(np.append(p3.get_ydata(),bestpars[1]))
 
-        #update limits
-        bx.set_xlim(left = y1.min(), right = y1.max())
-        cx.set_xlim(left = y1.min(), right = y1.max())
-        bx.set_ylim(bottom = np.min(p2.get_ydata()), top = np.max(p2.get_ydata()))
-        cx.set_ylim(bottom = np.min(p3.get_ydata()), top = np.max(p3.get_ydata()))
-        dx.set_xlim(left = new_data[5][0]/1000, right = new_data[5][-1]/1000)
-        ex.set_xlim(left = new_data[5][0]/1000, right = new_data[5][-1]/1000)
-        fx.set_xlim(left = new_data[5][0]/1000, right = new_data[5][-1]/1000)
-        dx.set_ylim(bottom = np.min(new_data[2]), top = np.max(new_data[2]))
-        ex.set_ylim(bottom = np.min(new_data[3]), top = np.max(new_data[3]))
-        fx.set_ylim(bottom = np.min(new_data[4]), top = np.max(new_data[4]))
-        if parameters[8]:
-            ax.set_xlim(left = 0, right = times[-1])
-            ax.set_ylim(bottom = y1.min(), top = y1.max())
-        else:
-            if len(parameters[9]) == 2:
-                t0 = float(parameters[9][0])
-                if t0> times[-1]:
-                    t0 = times[-1]-.5
-                t1 = float(parameters[9][1])
-                inds = np.logical_and(times >= t0, times <= t1)
-                ax.set_xlim(left = t0,right = t1)
-            elif len(parameters[9]) == 1:
-                t0 = float(parameters[9][0])
-                if t0> times[-1]:
-                    t0 = times[-1]-.5
-                inds = np.logical_not(times<t0)
-                ax.set_xlim(left = t0,right = times[-1])
-            else:
-                inds = np.logical_not(times<0)
-                ax.set_xlim(left = 0, right = times[-1])
-            ax.set_ylim(bottom = y1[inds].min(), top = y1[inds].max())
+        # #update limits
+        # bx.set_xlim(left = y1.min(), right = y1.max())
+        # cx.set_xlim(left = y1.min(), right = y1.max())
+        # bx.set_ylim(bottom = np.min(p2.get_ydata()), top = np.max(p2.get_ydata()))
+        # cx.set_ylim(bottom = np.min(p3.get_ydata()), top = np.max(p3.get_ydata()))
+        # dx.set_xlim(left = new_data[5][0]/1000, right = new_data[5][-1]/1000)
+        # ex.set_xlim(left = new_data[5][0]/1000, right = new_data[5][-1]/1000)
+        # fx.set_xlim(left = new_data[5][0]/1000, right = new_data[5][-1]/1000)
+        # dx.set_ylim(bottom = np.min(new_data[2]), top = np.max(new_data[2]))
+        # ex.set_ylim(bottom = np.min(new_data[3]), top = np.max(new_data[3]))
+        # fx.set_ylim(bottom = np.min(new_data[4]), top = np.max(new_data[4]))
+        # if parameters[8]:
+        #     ax.set_xlim(left = 0, right = times[-1])
+        #     ax.set_ylim(bottom = y1.min(), top = y1.max())
+        # else:
+        #     if len(parameters[9]) == 2:
+        #         t0 = float(parameters[9][0])
+        #         if t0> times[-1]:
+        #             t0 = times[-1]-.5
+        #         t1 = float(parameters[9][1])
+        #         inds = np.logical_and(times >= t0, times <= t1)
+        #         ax.set_xlim(left = t0,right = t1)
+        #     elif len(parameters[9]) == 1:
+        #         t0 = float(parameters[9][0])
+        #         if t0> times[-1]:
+        #             t0 = times[-1]-.5
+        #         inds = np.logical_not(times<t0)
+        #         ax.set_xlim(left = t0,right = times[-1])
+        #     else:
+        #         inds = np.logical_not(times<0)
+        #         ax.set_xlim(left = 0, right = times[-1])
+        #     ax.set_ylim(bottom = y1[inds].min(), top = y1[inds].max())
 
-        plt.pause(0.031)#show plot
+        # plt.pause(0.031)#show plot
 
         
 
